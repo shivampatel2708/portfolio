80739038887b352c1980b62239f048e8a8aa0aed
shivampatel2708 <shivampatel2708@gmail.com>
2017-02-19 23:59:02 +0530

    start

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<title>Shivam Patel</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="css/w3.css">
+<link href="css/style.css" rel="stylesheet" type="text/css" />
+<body>
+
+<div>
+  <ul class="w3-navbar ">
+    <li>
+      <a href="index.php" class="w3-margin-left"><b>Shivam </b> Patel</a>
+    </li>
+    <li class="w3-right w3-hide-small">
+      <a href="#projects" class="w3-left">Projects</a>
+      <a href="#resume" class="w3-left w3-margin-right">Resume</a>
+      <a href="#about" class="w3-left">About</a>
+      <a href="#contact" class="w3-left">Contact</a>
+      <a href="#work" class="w3-left">Work</a>
+    </li>
+  </ul>
+</div>
+
+<div class="w3-container " style="background-image: url('img/banner.jpg') !important;">
+	<h1 class="introtitle w3-opacity">Hello</h1>
+	<h2 class="w3-opacity name w3-center" >My Name is <b style="font-size: 88px;">Shivam Patel</b>
+	<br></h2>
+</div>
+
+<div class="w3-container fix" >
+	<h1 class="sectiontitle w3-opacity">Latest Work</h1>
+	<h2 class="w3-opacity headings w3-center">Currently I am working at LegalRaasta Technologies Private Limited. <br>I love to code<br>Currently living in New Delhi, India</h2>
+</div>
+
+<div class="w3-container height" style="background-image: url('img/work.jpg');">
+	<h1 class="sectiontitle1 w3-opacity" >My Expertise</h1>
+	<h2 class="w3-opacity headings1 w3-center" >All that is not nature is Design</h2>
+	<p class="myself w3-opacity">I have a wide range of skills that include design (and working closely with designers), front-end development (HTML5, CSS3, Javascript,Bootstrap3, Jquery,AngularJS, Material UI, Sematic UI, Responsive Web Design, UX), back-end development using open source technologies (PHP, Java, C, C++), Linux , load balancing, deployment and performance tuning (Apache, MySQL and NGinx). Apart From these technologies, I have also worked on SoapUI which is an open-source web service testing application for service-oriented architectures (SOA) and representational state transfers (REST).Also worked on Java frameworks like Spring and Hibernate and have worked on build automation tools like apache maven.</p>
+</div>
+
+</body>
+</html>
